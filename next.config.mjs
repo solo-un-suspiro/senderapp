556c9b8ffa030dee94b40de8e11f38e008b8966a
solo-un-suspiro <soporte.nexwey.td@gmail.com>
2025-03-20 18:42:46 +0000

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Configuración para permitir la ejecución de procesos secundarios
-    experimental: {
-      serverComponentsExternalPackages: ["@whiskeysockets/baileys", "qrcode-terminal"],
-    },
-    // Configuración para Render
-    output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
-  }
-  
-  module.exports = nextConfig
-  
-  
+  // Configuración para permitir la ejecución de procesos secundarios
+  experimental: {
+    serverComponentsExternalPackages: ["@whiskeysockets/baileys", "qrcode-terminal"],
+  },
+  // Configuración para Render
+  output: "standalone",
+}
+
+module.exports = nextConfig
+
